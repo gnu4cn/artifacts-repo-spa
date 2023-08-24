c128d133e7ac5d21788bfb3bdf2dc3dd038ff09d
rust-lang.xfoss.com <unisko@gmail.com>
2023-08-24 18:47:02 +0800

    Finished a git diffs url shorten function.

--- src/app/changelog.ts
+++ src/app/changelog.ts
@@ -2,6 +2,6 @@ export interface Changelog {
     id: number;
     commit_id: string;
     commit_comment: string;
-    commit_by: string;
+    commited_by: string;
     release_id: number;
 }
--- src/app/release/release.component.ts
+++ src/app/release/release.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { ReleaseDTO } from '../release';
 import { ChangelogComponent } from '../changelog/changelog.component';
@@ -10,6 +10,22 @@ import { ArtifactComponent } from '../artifact/artifact.component';
   templateUrl: './release.component.html',
   styleUrls: ['./release.component.css'],
 })
-export class ReleaseComponent {
+export class ReleaseComponent implements OnInit {
     @Input() rel_dto?: ReleaseDTO;
+
+    demo: string = 'https://github.com/Senscomm/wise/compare/e892f5fad8bc105ab9324cf110ac2dd352ca7170..6aebe3773235a7bcbfddddeb2ae3d4d03624b000#diff';
+
+    getShortDiffsUrl(url: string): string {
+        let raw = url.split('/').pop() as string;
+        let output = url.replace(raw, '');
+
+        let commitIds = raw.split('..');
+
+        let previousCommitId = commitIds[0].slice(0, 6);
+        let currentCommitId = commitIds[1].slice(0, 6);
+
+        return output.concat(previousCommitId, '..', currentCommitId, '#diffs')
+    }
+
+    ngOnInit() {}
 }
